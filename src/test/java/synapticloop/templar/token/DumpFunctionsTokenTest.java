83eb9d491ddb59b6f6a87cf9198c7a911bb9fe46
Synapticloop <osmanj@15-Inches-of-Fun.local>
2015-08-24 12:58:36 +0100

    added in filestatistics

--- src/test/java/synapticloop/templar/token/DumpFunctionsTokenTest.java
+++ src/test/java/synapticloop/templar/token/DumpFunctionsTokenTest.java
@@ -3,6 +3,7 @@ package synapticloop.templar.token;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,52 +11,79 @@ import org.mockito.MockitoAnnotations;
 
 import synapticloop.templar.exception.ParseException;
 import synapticloop.templar.exception.RenderException;
+import synapticloop.templar.function.FunctionAnd;
+import synapticloop.templar.function.FunctionEqual;
+import synapticloop.templar.function.FunctionFalse;
+import synapticloop.templar.function.FunctionFormatDate;
+import synapticloop.templar.function.FunctionGreaterThan;
+import synapticloop.templar.function.FunctionGreaterThanEqual;
+import synapticloop.templar.function.FunctionIsNotNull;
+import synapticloop.templar.function.FunctionIsNull;
+import synapticloop.templar.function.FunctionLength;
+import synapticloop.templar.function.FunctionLessThan;
+import synapticloop.templar.function.FunctionLessThanEqual;
+import synapticloop.templar.function.FunctionNotEqual;
+import synapticloop.templar.function.FunctionOr;
+import synapticloop.templar.function.FunctionSize;
+import synapticloop.templar.function.FunctionTrue;
+import synapticloop.templar.function.math.FunctionAdd;
+import synapticloop.templar.function.math.FunctionDivide;
+import synapticloop.templar.function.math.FunctionModulus;
+import synapticloop.templar.function.math.FunctionMultiply;
+import synapticloop.templar.function.math.FunctionPower;
+import synapticloop.templar.function.math.FunctionSubtract;
 import synapticloop.templar.utils.TemplarContext;
 import synapticloop.templar.utils.Tokeniser;
 
 
 public class DumpFunctionsTokenTest {
-	DumpContextToken dumpContextToken;
+	DumpFunctionsToken dumpFunctionsToken;
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
 
-	@Test
-	public void testNullContext() {
-		try {
-			dumpContextToken = new DumpContextToken(null, null, new Tokeniser());
-			assertEquals("<DUMPCONTEXT@1:1 />", dumpContextToken.toString());
-		} catch (ParseException e) {
-			assertFalse(true);
-		}
-	}
 
 	@Test
 	public void testRender() throws ParseException, RenderException {
-		dumpContextToken = new DumpContextToken(null, null, new Tokeniser());
+		dumpFunctionsToken = new DumpFunctionsToken(null, null, new Tokeniser());
 		TemplarContext templarContext = new TemplarContext();
-		templarContext.add("key", "value");
-		assertEquals("TemplarContext[{key:value}]", dumpContextToken.render(templarContext));
-	}
-
-	@Test
-	public void testRenderObjects() throws ParseException, RenderException {
-		dumpContextToken = new DumpContextToken(null, null, new Tokeniser());
-		TemplarContext templarContext = new TemplarContext();
-		templarContext.add("key", "value");
-		templarContext.add("stringArray", new String[8]);
-		String render = dumpContextToken.render(templarContext);
-		assertTrue(render.contains("TemplarContext["));
-		assertTrue(render.contains("{stringArray:[Ljava.lang.String;"));
-		assertTrue(render.contains("{key:value}"));
-	}
-
-	@Test
-	public void testRenderNullContext() throws ParseException, RenderException {
-		dumpContextToken = new DumpContextToken(null, null, new Tokeniser());
-		assertEquals("TemplarContext[]", dumpContextToken.render(null));
+		String render = dumpFunctionsToken.render(templarContext);
+		assertTrue(render.contains(FunctionLessThanEqual.class.getName()));
+		assertTrue(render.contains(FunctionLessThanEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionNotEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionLessThan.class.getSimpleName()));
+		assertTrue(render.contains(FunctionAnd.class.getSimpleName()));
+		assertTrue(render.contains(FunctionGreaterThanEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionLessThanEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionPower.class.getSimpleName()));
+		assertTrue(render.contains(FunctionOr.class.getSimpleName()));
+		assertTrue(render.contains(FunctionIsNotNull.class.getSimpleName()));
+		assertTrue(render.contains(FunctionFormatDate.class.getSimpleName()));
+		assertTrue(render.contains(FunctionModulus.class.getSimpleName()));
+		assertTrue(render.contains(FunctionAnd.class.getSimpleName()));
+		assertTrue(render.contains(FunctionLength.class.getSimpleName()));
+		assertTrue(render.contains(FunctionFalse.class.getSimpleName()));
+		assertTrue(render.contains(FunctionNotEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionMultiply.class.getSimpleName()));
+		assertTrue(render.contains(FunctionAdd.class.getSimpleName()));
+		assertTrue(render.contains(FunctionIsNotNull.class.getSimpleName()));
+		assertTrue(render.contains(FunctionGreaterThan.class.getSimpleName()));
+		assertTrue(render.contains(FunctionSubtract.class.getSimpleName()));
+		assertTrue(render.contains(FunctionDivide.class.getSimpleName()));
+		assertTrue(render.contains(FunctionEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionIsNull.class.getSimpleName()));
+		assertTrue(render.contains(FunctionSize.class.getSimpleName()));
+		assertTrue(render.contains(FunctionTrue.class.getSimpleName()));
+		assertTrue(render.contains(FunctionNotEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionNotEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionLessThan.class.getSimpleName()));
+		assertTrue(render.contains(FunctionOr.class.getSimpleName()));
+		assertTrue(render.contains(FunctionEqual.class.getSimpleName()));
+		assertTrue(render.contains(FunctionIsNotNull.class.getSimpleName()));
+		assertTrue(render.contains(FunctionGreaterThan.class.getSimpleName()));
+		assertTrue(render.contains(FunctionGreaterThanEqual.class.getSimpleName()));
 	}
 
 }
